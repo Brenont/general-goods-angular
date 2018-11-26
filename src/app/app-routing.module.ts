e5bb750457f307a898fdd2246a684bf123feb8a8
Breno Nunes <Brenontavares@gmail.com>
2018-11-26 20:41:48 -0200

    routing config to all containers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './containers/home/home.component';
+import { ProdutosComponent } from './containers/produtos/produtos.component';
+import { QmsomosComponent } from './containers/qmsomos/qmsomos.component';
+import { ContatoComponent } from './containers/contato/contato.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'produtos', component: ProdutosComponent},
+  {path: 'quem-somos', component: QmsomosComponent},
+  {path: 'contato', component: ContatoComponent},
 ];
 
 @NgModule({

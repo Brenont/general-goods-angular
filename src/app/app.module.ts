5925a85424c69c02048348fa7205f712660d7c5d
Igor Pereira <igorpereirapoliceno@gmail.com>
2018-11-27 08:07:41 -0200

    create component contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './containers/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ContactComponent } from './containers/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,

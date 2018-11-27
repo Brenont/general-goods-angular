375d1714a7191dda2419eb82d4121331e81f0adf
Igor Pereira <igorpereirapoliceno@gmail.com>
2018-11-27 12:07:48 -0200

    Contato screen --WIP

--- src/app/theme/pt.js
+++ src/app/theme/pt.js
@@ -3,6 +3,7 @@ export const LANG = {
     //CONTATO SCEEN
     faleConosco : "FALE CONOSCO",
     email : "generalgoods@generalgoods.com",
+    emailTitle : "NOSSO EMAIL",
     telTitle : "TELEFONE",
     tel : "(21) 2281-1319",
 }

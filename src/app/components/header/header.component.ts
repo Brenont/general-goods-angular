1cb720145427dc2f26661b0e969a4ab8eebc8e46
Breno Nunes <Brenontavares@gmail.com>
2018-11-27 18:04:40 -0200

    active menu --WIP

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  @Input() active :string;
 
   constructor() { }
 

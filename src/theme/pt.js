b2c85bd5c2664aa07b181b27aa640dff9654ba72
PabloHonorato <pblhonorato@gmail.com>
2018-12-03 17:25:40 -0200

    variaveis txt qmsomos

--- src/theme/pt.js
+++ src/theme/pt.js
@@ -12,4 +12,22 @@ export const LANG = {
     productsTitle : "Produtos",
     companyTitle : "Quem Somos",
     contactTitle : "Contato",
+
+    //QUEM SOMOS
+    title : "GENERAL GOODS",
+    subTitle : "A EMPRESA",
+    text1 : "Criada em 1993, a General Goods é uma empresa focada em atender a demanda das empresas por produto de qualidade voltados para o auxílio na logística e transporte. Atualmente possuímos atividades voltadas para 5 seguimentos Caixas de Isopor para transporte de medicamentos; Caixa de PVC, Gelo Reciclável; Paletes Plásticos e Containers para armazenamento de gelo seco. Contando com 26 produtos próprios, desenvolvidos com tecnologiade ponta para atender as necessidades dos nossos clientes.",
+    text2 :"Como diferencial, apresentamos um alto nível de personalização dos produtos desenvolvidos. O setor comercial é altamente treinado para estudar os fluxos logísticos do cliente e oferecer assim, o produto mais adequado às suas necessidades, mesmo que para isso seja necessário mudar a linha de produção.",
+    subTitle2 : "ALGUNS CLIENTES",
+    parcerias1 : "Casa da Moeda do Brasil",
+    parcerias2: "Biomanguinhos",
+    parcerias3 : "Fundação Oswaldo Cruz",
+    parcerias4 : "Farmanguinhos",
+    parcerias5 : "Laboratório Farmacêutico da Marinha do Brasil",
+    parcerias6 : "Laboratório Farmacêutico da Aeronáutica",
+
+
+  
+    
+    
 }

07c005a058067841fcbe50fbde226abdf764b556
Ana Murdock <anamurdock15@gmail.com>
2018-12-08 13:12:46 -0200

    Responsividade do Carrossel

--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { LANG } from '../../../theme/pt'
-
 
 @Component({
   selector: 'app-home',
@@ -12,6 +11,7 @@ import { LANG } from '../../../theme/pt'
 export class HomeComponent implements OnInit {
 
   public lang = LANG;
+  public innerWidth;
 
   constructor() { }
 
@@ -23,36 +23,29 @@ export class HomeComponent implements OnInit {
   }
 
   slides = [
-    {img: "../../../assets/img/eco.jpg", textOne: true, textTwo: false},
-    {img: "../../../assets/img/isopor.jpg",  textOne: false, textTwo: true},
-    {img: "../../../assets/img/eco.jpg", textOne: true, textTwo: false},
-    {img: "../../../assets/img/isopor.jpg", textOne: false, textTwo: true}
+    { img: "../../../assets/img/eco.jpg", textOne: true, textTwo: false },
+    { img: "../../../assets/img/isopor.jpg", textOne: false, textTwo: true },
+    { img: "../../../assets/img/eco.jpg", textOne: true, textTwo: false },
+    { img: "../../../assets/img/isopor.jpg", textOne: false, textTwo: true }
   ];
-  
+
+  slidesMobile = [
+    { img: "../../../assets/img/eco-mob.jpg", textOne: true, textTwo: false },
+    { img: "../../../assets/img/isopor-mob.jpg", textOne: false, textTwo: true },
+    { img: "../../../assets/img/eco-mob.jpg", textOne: true, textTwo: false },
+    { img: "../../../assets/img/isopor-mob.jpg", textOne: false, textTwo: true }
+  ];
+
+
   slideConfig = {
     arrows: true,
     dots: true,
     autoplay: true,
     infinite: true,
-    slidesToShow: 1, 
+    slidesToShow: 1,
     slidesToScroll: 1,
-    initialSlide: 0
-  };
+    initialSlide: 0,
 
-  slickInit(e) {
-    console.log('slick initialized');
-  }
-  
-  breakpoint(e) {
-    console.log('breakpoint');
-  }
-  
-  afterChange(e) {
-    console.log('afterChange');
-  }
-  
-  beforeChange(e) {
-    console.log('beforeChange');
-  }
+  };
 }
-  
+

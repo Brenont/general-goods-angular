91237885ffc627171d906b65cacb19d6baebd7cc
Igor Pereira <igorpereirapoliceno@gmail.com>
2018-12-11 03:12:25 -0200

    carrinho desktop

--- src/app/containers/carrinho/carrinho.component.ts
+++ src/app/containers/carrinho/carrinho.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
   styleUrls: ['./carrinho.component.scss']
 })
 export class CarrinhoComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {

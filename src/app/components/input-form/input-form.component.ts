7c89c4bd221033850f62adea27100ed97949ffb9
Breno Nunes <Brenontavares@gmail.com>
2018-12-12 10:49:46 -0200

    input-form && icon back of login-screen

--- src/app/components/input-form/input-form.component.ts
+++ src/app/components/input-form/input-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
+// import { type } from 'os';
 
 @Component({
   selector: 'app-input-form',
@@ -6,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-form.component.scss']
 })
 export class InputFormComponent implements OnInit {
+
+  @Input() placeHolder : string;
+  @Input() id :string;
+  @Input() name :string;
+  @Input() type :string;
 
   constructor() { }
 

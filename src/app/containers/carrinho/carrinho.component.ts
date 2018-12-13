8b78ac3dd6f939226510516d0a0833dce5b69834
Breno Nunes <Brenontavares@gmail.com>
2018-12-13 10:49:04 -0200

    Adding object in localStorage --WIP

--- src/app/containers/carrinho/carrinho.component.ts
+++ src/app/containers/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/model/product';
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
@@ -7,6 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class CarrinhoComponent implements OnInit {
 
   public qtdOptions = [];
+  public carrinhoItens: Produto[];
+
+  public testObj = {
+    name: "Caixa Térmica",
+    descriptions: [
+      "Caixa apropriada para transportar produtos perecíveis à longa distância.",
+    ],
+    features: [
+      "Feito em polietileno, resistente aos raios UV.",
+      "Solado com poliuretano para conservar temperatura por mais tempo.",
+      "Material robusto, resistente à impactos.",
+      "Travas de aço inox.*",
+      "Pedais de borracha antiderrapante.*",
+      "Dreno.*",
+      "Travas de borracha.*",
+      "Rodizios com travas e rodas de borracha antiderrapantes.*",
+    ],
+    sizes: null,
+    extras: [
+      "*Itens opcionais, personalizadas de acordo com necessidade do cliente.",
+    ],
+    img: "../../../assets/img/caixatermica.png",
+    customizes: [
+      "Travas de aço inox",
+      "Pedais de borracha antiderrapante",
+      "Dreno",
+      "Travas de borracha",
+      "Rodizios com travas e rodas de borracha antiderrapantes"],
+    nucleoOption: null,
+  }
 
   constructor() { }
 
@@ -14,6 +45,8 @@ export class CarrinhoComponent implements OnInit {
     for (let i = 1; i < 100; i++) {
       this.qtdOptions.push(i);
     }
+    var objString = this.testObj.toString();
+    localStorage.setItem('test2', objString);
   }
-
 }
+

a3826578205c3a86e3a79d53bdf782d381bcf501
Breno Nunes <Brenontavares@gmail.com>
2018-12-13 18:23:39 -0200

    Cart methods --WIP

--- src/app/containers/carrinho/carrinho.component.ts
+++ src/app/containers/carrinho/carrinho.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from 'src/app/model/product';
+import { produtos } from 'src/app/services/produto.service';
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
@@ -8,45 +9,47 @@ import { Produto } from 'src/app/model/product';
 export class CarrinhoComponent implements OnInit {
 
   public qtdOptions = [];
-  public carrinhoItens: Produto[];
 
-  public testObj = {
-    name: "Caixa Térmica",
-    descriptions: [
-      "Caixa apropriada para transportar produtos perecíveis à longa distância.",
-    ],
-    features: [
-      "Feito em polietileno, resistente aos raios UV.",
-      "Solado com poliuretano para conservar temperatura por mais tempo.",
-      "Material robusto, resistente à impactos.",
-      "Travas de aço inox.*",
-      "Pedais de borracha antiderrapante.*",
-      "Dreno.*",
-      "Travas de borracha.*",
-      "Rodizios com travas e rodas de borracha antiderrapantes.*",
-    ],
-    sizes: null,
-    extras: [
-      "*Itens opcionais, personalizadas de acordo com necessidade do cliente.",
-    ],
-    img: "../../../assets/img/caixatermica.png",
-    customizes: [
-      "Travas de aço inox",
-      "Pedais de borracha antiderrapante",
-      "Dreno",
-      "Travas de borracha",
-      "Rodizios com travas e rodas de borracha antiderrapantes"],
-    nucleoOption: null,
-  }
+  public storageName :string = "cart";
+
+  private productsArray = produtos;
+  public cartArray = JSON.parse(localStorage.getItem(this.storageName));
+  
+
 
   constructor() { }
 
   ngOnInit() {
+    this.createOptions();
+    
+    //converting object to string and set on LS
+    localStorage.setItem(this.storageName, JSON.stringify(this.productsArray));
+
+  }
+
+  addToCart(_index) {
+    var productToAdd = this.cartArray[_index];
+
+    var oldArray = this.cartArray;
+    var oldArray = oldArray.push(productToAdd);
+    console.log(this.cartArray);
+    this.updateStorage(this.cartArray);
+  }
+
+  deleteProduct(_index) {
+    var deleted = this.cartArray.splice(_index, -1);
+    this.updateStorage(deleted);
+  }
+
+  updateStorage(update){
+    var updateString = JSON.stringify(update);
+    localStorage.setItem(this.storageName, updateString);
+  }
+
+  //options of the select of quantify
+  createOptions() {
     for (let i = 1; i < 100; i++) {
       this.qtdOptions.push(i);
     }
-    var objString = this.testObj.toString();
-    localStorage.setItem('test2', objString);
   }
 }
-

442a7a73133d339ee7f885400ad3f741fbaa0b84
Breno Nunes <Brenontavares@gmail.com>
2019-03-20 13:42:21 -0300

    transfering upload photos
    from admin.ts to add-product.ts

--- src/app/containers/admin/add-product/add-product.component.ts
+++ src/app/containers/admin/add-product/add-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LANG } from '../../../../theme/pt'
 import { ProdutoService } from 'src/app/services/produto.service';
+import { UploadImgService } from 'src/app/services/upload-img.service';
 
 @Component({
   selector: 'app-add-product',
@@ -11,7 +12,7 @@ export class AddProductComponent implements OnInit {
 
   public lang = LANG;
 
-  constructor(private prodService: ProdutoService) { }
+  constructor(private prodService: ProdutoService, private uploadImgService: UploadImgService) { }
 
   add(product) {
     console.log("click to add")
@@ -20,6 +21,10 @@ export class AddProductComponent implements OnInit {
 
   generateKey(){
     return this.prodService.generateKey();
+  }
+
+  uploadFile(event){
+    this.uploadImgService.uploadFile(event);
   }
 
   ngOnInit() {
